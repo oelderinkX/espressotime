dde07d6ff2d812ff83c4f40383f4afdd78d053d8
oelderink <joelderink.wale@gmail.com>
2021-03-06 13:58:48 +1300

    try da function, things seem to be happening

--- main.js
+++ main.js
@@ -62,7 +62,7 @@ module.exports = function(app){
 		var shopId = 1;
 		//var pass = req.body.pass;
 		//var employeeId = req.body.employeeId;
-		console.log('employee id: ' + req.body.employeeId);
+		console.log('req: ' + req);
 		var employeeId = 5;
 		
 		var sql = "SELECT id, name, contact from espresso.employee where shopid = $1;"
