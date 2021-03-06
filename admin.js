f1b97678a1505944dca65cc904c1ae2cd7a9ce1c
oelderink <joelderink.wale@gmail.com>
2021-03-06 13:15:05 +1300

    ad admin

--- admin.js
+++ admin.js
@@ -1,11 +1,11 @@
 var pg = require('pg');
-//var common = require('./script/common.js');
+var common = require('./script/common.js');
 var bodyParser = require('body-parser');
 var fs = require("fs");
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-//var pool = new pg.Pool(common.postgresConfig());
+var pool = new pg.Pool(common.postgresConfig());
 
 var adminPage = fs.readFileSync(__dirname + "/webpage/admin.html", "utf8");
 
