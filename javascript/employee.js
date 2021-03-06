d11ee49a98e4a29e7f184f3af3c6a25f20cbf977
oelderink <joelderink.wale@gmail.com>
2021-03-06 16:58:09 +1300

    better onclick

--- javascript/employee.js
+++ javascript/employee.js
@@ -40,7 +40,8 @@ function getEmployees() {
             var a1 = document.createElement("a");
             a1.setAttribute('href', '#');
             a1.innerHTML = employees[i].name;
-            a1.onclick = function() { getEmployeeDetails(employees[i].id) };
+            //a1.onclick = function() { getEmployeeDetails(employees[i].id) };
+            a1.setAttribute('onclick', 'getEmployeeDetails(' + employees[i].id + ');');
             li1.appendChild(a1);
             li1.classList.add('active');
             employeelist1.appendChild(li1);
@@ -49,7 +50,8 @@ function getEmployees() {
             var a2 = document.createElement("a");
             a2.setAttribute('href', '#');
             a2.innerHTML = employees[i].name;
-            a2.onclick = function() { getEmployeeDetails(employees[i].id) };
+            //a2.onclick = function() { getEmployeeDetails(employees[i].id) };
+            a2.setAttribute('onclick', 'getEmployeeDetails(' + employees[i].id + ');');
             li2.appendChild(a2);
             li2.classList.add('active');
             employeelist2.appendChild(li2);
