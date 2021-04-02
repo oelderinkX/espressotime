5de7246ef46108024fc6aee0e4f64f7dfccd1015
oelderink <joelderink.wale@gmail.com>
2021-04-03 12:05:08 +1300

    more changes

--- javascript/employee.js
+++ javascript/employee.js
@@ -26,30 +26,34 @@ function getEmployees() {
     sendPost("/getemployees", '', function(response) {
         var employees = JSON.parse(response);
 
-        var navbar1 = document.getElementById("navbar1");
-        var navbar2 = document.getElementById("navbar2");
+        var mobilenavbar = document.getElementById("mobilenavbar");
+        var webnavbar = document.getElementById("webnavbar");
 
-        var employeelist1 = document.getElementById("employeelist1");
-        var employeelist2 = document.getElementById("employeelist2");
+        var mobileemployeelist = document.getElementById("mobileemployeelist");
+        var webemployeelist = document.getElementById("webemployeelist");
 
         for(var i = 0; i < employees.length; i++) {
-            var li1 = document.createElement("li");
-            var a1 = document.createElement("a");
-            a1.setAttribute('href', '#');
-            a1.innerHTML = employees[i].name;
-            a1.setAttribute('onclick', 'getEmployeeDetails(' + employees[i].id + ');');
-            li1.appendChild(a1);
-            li1.classList.add('active');
-            employeelist1.appendChild(li1);
+            if (mobilenavbar.offsetHeight > 0) {
+                var li1 = document.createElement("li");
+                var a1 = document.createElement("a");
+                a1.setAttribute('href', '#');
+                a1.innerHTML = employees[i].name;
+                a1.setAttribute('onclick', 'getEmployeeDetails(' + employees[i].id + ');');
+                li1.appendChild(a1);
+                li1.classList.add('active');
+                mobileemployeelist.appendChild(li1);
+            }
 
-            var li2 = document.createElement("li");
-            var a2 = document.createElement("a");
-            a2.setAttribute('href', '#');
-            a2.innerHTML = employees[i].name;
-            a2.setAttribute('onclick', 'getEmployeeDetails(' + employees[i].id + ');');
-            li2.appendChild(a2);
-            li2.classList.add('active');
-            employeelist2.appendChild(li2);
+            if (webnavbar.offsetHeight > 0) {
+                var li2 = document.createElement("li");
+                var a2 = document.createElement("a");
+                a2.setAttribute('href', '#');
+                a2.innerHTML = employees[i].name;
+                a2.setAttribute('onclick', 'getEmployeeDetails(' + employees[i].id + ');');
+                li2.appendChild(a2);
+                li2.classList.add('active');
+                webemployeelist.appendChild(li2);
+            }
         }
     });
 }
@@ -182,6 +186,14 @@ function getEmployeeDetails(employeeId) {
             contact.classList.add("invisible");
             starttime.classList.add("invisible");
             finishtime.classList.add("invisible");
+            breaks.classList.add("invisible");
+            shiftbutton.classList.add("invisible");
+            restButton.classList.add("invisible");
+            mealButton.classList.add("invisible");
+        }
+
+        var mobilenavbar = document.getElementById("mobilenavbar");
+        if (mobilenavbar.offsetHeight > 0) {
             breaks.classList.add("invisible");
             shiftbutton.classList.add("invisible");
             restButton.classList.add("invisible");
