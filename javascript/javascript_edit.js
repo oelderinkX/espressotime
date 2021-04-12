3dc064080dfbf6aa8e36520a3fd2027e87250d44
oelderink <joelderink.wale@gmail.com>
2021-04-12 19:54:29 +1200

    yerp

--- javascript/javascript_edit.js
+++ javascript/javascript_edit.js
@@ -14,7 +14,7 @@ function getEmployees() {
 }
 
 function setDates() {
-    var day = document.getElementById('employeecombox');
+    var day = document.getElementById('day');
     var month = document.getElementById('month');
     var year = document.getElementById('year');
 
