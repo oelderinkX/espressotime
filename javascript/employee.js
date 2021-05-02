2c90912a47e3548260737bb36b25f02b587742be
oelderink <joelderink.wale@gmail.com>
2021-05-02 21:34:32 +1200

    3 spaces

--- javascript/employee.js
+++ javascript/employee.js
@@ -138,16 +138,16 @@ function getEmployeeDetails(employeeId) {
                 if (bStartTime) {
                     if (bFinishTime) {
                         if (bBreakType == 10) {
-                            breaks10mins += '&nbsp;&nbsp;&nbsp;<span class="glyphicon glyphicon-time"></span> ' + formatTime(bStartTime) + '-' + formatTime(bFinishTime) + ' (' + calculateMinutes(bStartTime, bFinishTime) + ' mins) <br/>';
+                            breaks10mins += '&nbsp;&nbsp;<span class="glyphicon glyphicon-time"></span> ' + formatTime(bStartTime) + '-' + formatTime(bFinishTime) + ' (' + calculateMinutes(bStartTime, bFinishTime) + ' mins) <br/>';
                         } else if (bBreakType == 30) {
-                            breaks30mins += '&nbsp;&nbsp;&nbsp;<span class="glyphicon glyphicon-cutlery"></span> ' + formatTime(bStartTime) + '-' + formatTime(bFinishTime) + ' (' + calculateMinutes(bStartTime, bFinishTime) + ' mins) <br/>';
+                            breaks30mins += '&nbsp;&nbsp;<span class="glyphicon glyphicon-cutlery"></span> ' + formatTime(bStartTime) + '-' + formatTime(bFinishTime) + ' (' + calculateMinutes(bStartTime, bFinishTime) + ' mins) <br/>';
                         }
                     } else {
                         if (bBreakType == 10) {
-                            breaks10mins += '&nbsp;&nbsp;&nbsp;<span class="glyphicon glyphicon-time"></span> ' + formatTime(bStartTime) + '- <br/>';
+                            breaks10mins += '&nbsp;&nbsp;<span class="glyphicon glyphicon-time"></span> ' + formatTime(bStartTime) + '- <br/>';
                             on10minBreak = true;
                         } else if (bBreakType == 30) {
-                            breaks30mins += '&nbsp;&nbsp;&nbsp;<span class="glyphicon glyphicon-cutlery"></span> ' + formatTime(bStartTime) + '- <br/>';
+                            breaks30mins += '&nbsp;&nbsp;<span class="glyphicon glyphicon-cutlery"></span> ' + formatTime(bStartTime) + '- <br/>';
                             on30minBreak = true;
                         }
                     }
