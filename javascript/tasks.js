9dbe49d49cbead5c26d6a492324f1818359188d5
oelderink <joelderink.wale@gmail.com>
2021-08-20 22:31:08 +1200

    make the pass

--- javascript/tasks.js
+++ javascript/tasks.js
@@ -43,7 +43,7 @@ function getTasksForHour() {
     var date = getDbFormat();
     var request = {date: date, timestamp: timestamp};
 
-    sendPost("/gettasks", request, function(response) {
+    sendPost("/gettasks", JSON.parse(request), function(response) {
         var tasks = JSON.parse(response);
 
         var tasksarea = document.getElementById("tasksarea")
