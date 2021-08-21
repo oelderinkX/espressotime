58c7dcf3dc188fb7d82dc78c5a0687bc2b99ed3b
oelderink <joelderink.wale@gmail.com>
2021-08-21 20:09:00 +1200

    t success

--- javascript/javascript_admin.js
+++ javascript/javascript_admin.js
@@ -633,7 +633,7 @@ function addTask() {
     } else if (starttime.length != 0 && timeRegEx.test(starttime) == false) {
         alert('Incorrect start time of task.  Use format like 09:00:00 for 9am, or 13:30:00 for 1:30pm.  Leave empty to delete task');
     } else {
-        var json = { "name": name, "description": description, "starttime": startime };
+        var json = { "name": name, "description": description, "starttime": starttime };
 
         sendPost("/addtask", JSON.stringify(json), function(response) {
             getTasks();
