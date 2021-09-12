b878e43b68afe6fe1094d9cd022a215d8362ae8a
oelderink <joelderink.wale@gmail.com>
2021-09-12 22:02:55 +1200

    can't get better than perfoction

--- javascript/common.js
+++ javascript/common.js
@@ -110,8 +110,8 @@ function getTime(d) {
 
 function formatAMPM(dateStr) {
   var timeSplit = dateStr.split(':');
-  var hours = timeSplit[0];
-  var minutes = timeSplit[1];
+  var hours = parseInt(timeSplit[0]);
+  var minutes = parseInt(timeSplit[1]);
   var ampm = hours >= 12 ? 'pm' : 'am';
   hours = hours % 12;
   hours = hours ? hours : 12; // the hour '0' should be '12'
