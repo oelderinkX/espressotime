d59ce93e72635af6e8f17427e5f8024bd9abca45
oelderink <joelderink.wale@gmail.com>
2021-10-09 01:13:20 +1300

    might work

--- admin.js
+++ admin.js
@@ -145,6 +145,27 @@ module.exports = function(app){
 		res.send(webpage);
 	});	
 
+	app.post('/givefeedback', jsonParser, function(req, res) {
+		//var json = { shopId: shopId, feedbackitems: feedbackitems, additional: additional.value, timestamp: timestamp };
+
+		var shopId = req.body.shopId;
+		var feedbackitems = req.body.feedbackitems;
+		var additional = req.body.additional;
+		var timestamp = req.body.timestamp;
+
+		var sql = 'insert into espresso.feedback ';
+		sql += '(shopid, description0, rating0, description1, rating1, description2, rating2, description3, rating3, additional, timestamp)';
+		sql += 'values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)';
+
+		pool.connect(function(err, connection, done) {
+			connection.query(sql, [shopId, feedbackitems[0].description, feedbackitems[0].rating, feedbackitems[1].description, feedbackitems[1].rating, feedbackitems[2].description, feedbackitems[2].rating, feedbackitems[3].description, feedbackitems[3].rating, additional, timestamp], function(err, result) {
+				done();
+				var result = { "result": "success" };
+				res.send(result);
+			});
+		});
+	});
+
 	app.post('/admin_getemployees', jsonParser, function(req, res) {
 		var shopId = common.getShopId(req.cookies['identifier']);
 		var showEx = req.body.showEx;
