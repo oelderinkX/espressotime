f11eef17b5597e71387089eb0a412ba400d817e9
oelderink <joelderink.wale@gmail.com>
2022-04-01 22:02:29 +1300

    might work

--- javascript/options.js
+++ javascript/options.js
@@ -1,11 +1,9 @@
 var shopOptions = {};
 
 function updateShopOptions() {
-    if (!shopOptions || shopOptions == {}) {
-        sendPost("/get_shop_options", JSON.stringify('{}'), function(response) {
-            shopOptions = JSON.parse(response);
-        });
-    }
+    sendPost("/get_shop_options", JSON.stringify('{}'), function(response) {
+        shopOptions = JSON.parse(response);
+    });
 }
 
 function option_allowSingleClickBreaks() {
