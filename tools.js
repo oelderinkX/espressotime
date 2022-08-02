56c9cc4ec09a99c882f477a63e0e9fa9f0e7ab91
oelderink <joelderink.wale@gmail.com>
2022-08-02 18:55:48 +1200

    remove logging

--- tools.js
+++ tools.js
@@ -307,13 +307,7 @@ module.exports = function(app){
 							if (result && result.rowCount > 0) {
 								for(var i = 0; i < result.rowCount; i++) {
 									for(var x = 0; x < employeestimes.length; x++) {
-										console.log('employeestimes[x].id: ' + employeestimes[x].id);
-										console.log('result.rows[i].employeeid: ' + result.rows[i].employeeid);
-										console.log('equals: ' + (employeestimes[x].id == result.rows[i].employeeid));
-										console.log('');
-										
 										if (employeestimes[x].id == result.rows[i].employeeid) {
-
 											var date = new Date(result.rows[i].date);
 											var dateStr = dateHelper.formatDate(date);
 
@@ -322,7 +316,7 @@ module.exports = function(app){
 
 											var end = new Date(result.rows[i].finish);
 											var endStr = dateHelper.formatTime(end);
-
+											
 											employeestimes[x].times.push({
 												date: dateStr,
 												start: startStr,
