7a1be1cd7aabd74c33d8824ffc5e885337fc582b
oelderink <joelderink.wale@gmail.com>
2022-08-03 22:09:37 +1200

    all done ?

--- javascript/javascript_roster.js
+++ javascript/javascript_roster.js
@@ -114,7 +114,11 @@ function getEmployeeRow(employeetimes) {
         // Add the ondragstart event listener
         control.addEventListener("dragstart", dragstart_handler);
       });
+    } else {  // has no date so is dropable
+      control.setAttribute('ondragover', 'dragover_handler(event)');
+      control.setAttribute('ondrop', 'drop_handler(event)');
     }
+    
     var modal = createModal(modaltarget, employeetimes, rosterdates[i]);
 
     cell.appendChild(control);
@@ -357,6 +361,18 @@ function updateTime(employeeid, date, start, finish, role) {
 function dragstart_handler(ev) {
   // Add the target element's id to the data transfer object
   ev.dataTransfer.setData("text/plain", ev.target.id);
+}
+
+function dragover_handler(ev) {
+  ev.preventDefault();
+  ev.dataTransfer.dropEffect = "move";
+}
+
+function drop_handler(ev) {
+  ev.preventDefault();
+  // Get the id of the target and add the moved element to the target's DOM
+  const data = ev.dataTransfer.getData("text/plain");
+  //ev.target.appendChild(document.getElementById(data));
 }
 
 function dragdrop(src_id, src_date, src_modaltarget, dest_id, dest_date) {
