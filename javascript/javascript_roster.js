d71f5f0cc4e0e18c4f7acb09b4ece71514a8f30e
oelderink <joelderink.wale@gmail.com>
2022-08-09 21:00:06 +1200

    change colour

--- javascript/javascript_roster.js
+++ javascript/javascript_roster.js
@@ -3,12 +3,12 @@ var roles = [
   {"name": "Open", "colour": "#43A047", "textcolour": "white"},
   {"name": "Close", "colour": "#F4511E", "textcolour": "white"},
   {"name": "Sandwich", "colour": "#FFE0B2", "textcolour": "black"},
-  {"name": "Cook", "colour": "white", "textcolour": "black"},
+  {"name": "Cook", "colour": "yellow", "textcolour": "black"},
   {"name": "Sandwich/Cook", "colour": "#D7CCC8", "textcolour": "black"},
   {"name": "Dishwasher", "colour": "#4DD0E1", "textcolour": "black"},
   {"name": "Manager", "colour": "#2196F3", "textcolour": "white"},
   {"name": "Training", "colour": "#673AB7", "textcolour": "white"},
-  {"name": "Annual Leave", "colour": "#F48FB1", "textcolour": "black"},
+  {"name": "Annual Leave", "colour": "black", "textcolour": "white"},
   {"name": "Sick", "colour": "black", "textcolour": "white"}];
 
 function clearTable() {
