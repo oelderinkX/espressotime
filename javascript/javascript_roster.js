461362acc9b04ed4ca1e29030fc42263c98c8ee5
oelderink <joelderink.wale@gmail.com>
2022-08-22 21:07:30 +1200

    try again

--- javascript/javascript_roster.js
+++ javascript/javascript_roster.js
@@ -600,7 +600,7 @@ function copyLastWeek() {
 
   if (window.confirm("Are you sure you want to merge last weeks roster into this week ?")) {
     var request = { date: rosterdates[0], from_date: fromdate };
-    /*sendPost("/copylastweek", JSON.stringify(request), function(response) {
+    sendPost("/copylastweek", JSON.stringify(request), function(response) {
       var result =  JSON.parse(response);
 
       if (result.success == false) {
@@ -608,6 +608,6 @@ function copyLastWeek() {
       }
 
       getEmployeeTimes();
-    });*/
+    });
   }
 }
