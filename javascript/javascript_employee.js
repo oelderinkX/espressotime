db03ec4daa6d662fd74578e4483638c12b87dc40
oelderink <joelderink.wale@gmail.com>
2022-11-29 22:13:38 +1300

    make nice

--- javascript/javascript_employee.js
+++ javascript/javascript_employee.js
@@ -159,17 +159,22 @@ function loadBreaks() {
         var startbreak = document.createElement('td');
         startbreak.setAttribute('style', 'text-align: center; vertical-align: middle; height: 40px; width: 80px;');
         startbreak.innerHTML = formatAMPM(formatTime(breaks[i].starttime));
-      
+
+        var sep = document.createElement('td');
+        sep.setAttribute('style', 'text-align: center; vertical-align: middle; height: 40px; width: 40px;');
+        sep.innerHTML = '-';
+
         var endbreak = document.createElement('td');
         endbreak.setAttribute('style', 'text-align: center; vertical-align: middle; height: 40px; width: 80px;');
         if (breaks[i].finishtime == '-') {
-          endbreak.innerHTML = breaks[i].finishtime;
+          endbreak.innerHTML = '';
         } else {
           endbreak.innerHTML = formatAMPM(formatTime(breaks[i].finishtime));
         }
         
         tr.appendChild(breaktype);
         tr.appendChild(startbreak);
+        tr.appendChild(sep);
         tr.appendChild(endbreak);
 
         breakstable.appendChild(tr);
