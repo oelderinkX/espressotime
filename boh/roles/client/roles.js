52afb719bed77c12d7bf3d6c0f8edfd06913a49b
oelderink <joelderink.wale@gmail.com>
2023-04-22 22:06:19 +1200

    one bit at a time

--- boh/roles/client/roles.js
+++ boh/roles/client/roles.js
@@ -94,7 +94,10 @@ function save() {
 
     sendPost("/updaterole", JSON.stringify(role), function(response) {
         loadRoles();
-        displayRole(response.roleid);
+
+        var json  = JSON.parse(response);
+        displayRole(json.roleid);
+        
         alert('Saved!');
     });
 }
