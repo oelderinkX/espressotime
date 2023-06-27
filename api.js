645c2f98b566a2c48affa556bb4c86be068792ea
oelderink <joelderink.wale@gmail.com>
2023-06-27 21:31:52 +1200

    for work

---
+++ api.js
@@ -0,0 +1,26 @@
+var fs = require("fs");
+var pg = require('pg');
+var express = require('express');
+var bodyParser = require('body-parser');
+var common = require('../../../common/srv/common.js');
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+var jsonParser = bodyParser.json();
+
+var pool = new pg.Pool(common.postgresConfig());
+
+module.exports = function(app) {
+	var apiPage = fs.readFileSync(__dirname + "api.html", "utf8");
+
+	app.get('/api*', urlencodedParser, function(req, res) {
+        if (req.url.includes('/api/status')) {
+            res.send(apiPage);
+        } else {
+            res.send('{}');
+        }
+    });	
+
+	app.post('/api*', urlencodedParser, function(req, res) {
+        res.send('{}');
+    });	
+}
--- app.js
+++ app.js
@@ -73,3 +73,6 @@ require('./foh/tasks/srv/tasks.js')(app);
 
 console.log('loading rh_roster.js');
 require('./foh/roster/srv/foh_roster.js')(app);
+
+console.log('loading api.js');
+require('./api.js')(app);
