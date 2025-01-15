f7329d2574cfa211e7ed9ea5445da9851f0ecdb1
Jared <jared.oelderinkwale@gmail.com>
2025-01-15 21:53:52 +1300

    no more console logs lets look at speed now

--- common/srv/branding.js
+++ common/srv/branding.js
@@ -18,7 +18,6 @@ module.exports = function(app) {
 		if (shopid && shopid != -1) {
             let css = getCachedBranding(shopid);
             if (css !== null) {
-                console.log('branding.css - Cache Used!');
                 res.type('text/css');
                 res.send(css);
             } else {
@@ -33,7 +32,7 @@ module.exports = function(app) {
                             res.type('text/css');
                             res.send(result.rows[0].css);
                         }  else {
-                            console.log('branding.css - using basic css');
+                            setCachedBranding(shopid, basic_css);
                             res.type('text/css');
                             res.send(basic_css);
                         }
@@ -48,16 +47,11 @@ module.exports = function(app) {
 }
 
 function getCachedBranding(shopid) {
-    console.log('branding.css - getting cache');
-
     let now = new Date();
 
     for(let i = 0; i < cache_css.length; i++ ) {
-        console.log(cache_css[i].shopid + ' === ' + shopid + ' ?');
         if (cache_css[i].shopid === shopid) {
-            console.log('found shopid in cache, now: ' + now + ' expire: ' + cache_css[i].expire);
             if (now < cache_css[i].expire) {
-                console.log('branding.css - returning cache css: ' + cache_css[i].css);
                 return cache_css[i].css;
             }
         }
@@ -67,8 +61,6 @@ function getCachedBranding(shopid) {
 }
 
 function setCachedBranding(shopid, css) {
-    console.log('branding.css - setting cache');
-
     // clear old cached css
     for(let i = 0; i < cache_css.length; i++ ) {
         cache_css = cache_css.filter(c => c.shopid !== shopid);
@@ -76,8 +68,6 @@ function setCachedBranding(shopid, css) {
 
     let expire = new Date();
     expire.setMinutes(expire.getMinutes() + 3);
-
-    console.log('branding.css - setting cache css: ' + css);
 
     cache_css.push({
         shopid: shopid,
