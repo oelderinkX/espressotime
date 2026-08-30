960116062d4127e9c11e9083a183d1006e297bd8
Jared <jared.oelderinkwale@gmail.com>
2026-08-30 22:15:42 +1200

    first time no errors

--- boh/timeoffapprove/client/timeoffapprove.js
+++ boh/timeoffapprove/client/timeoffapprove.js
@@ -54,82 +54,90 @@ function loadEmployees() {
 }
 
 function displayAllTimeoffs() {
-    var employees = document.getElementById('employees');
-    var status = document.getElementById('status');
+    const employees = document.getElementById('employees');
+    const status = document.getElementById('status');
 
-    var timeoffs_table = document.getElementById('timeoffs');
+    const timeoffs_table = document.getElementById('timeoffs');
     timeoffs_table.innerHTML = '';
 
-    var row = document.createElement('tr');
+    const row = document.createElement('tr');
     row.style = 'background: white';
 
-    var heading = document.createElement('th');
-    heading.innerText = 'Employee Name';
-    row.appendChild(heading);
+    const heading1 = document.createElement('th');
+    heading1.innerText = 'Employee Name';
+    row.appendChild(heading1);
 
-    var heading = document.createElement('th');
-    heading.innerText = 'Time Off Dates';
-    row.appendChild(heading);
+    const heading2 = document.createElement('th');
+    heading2.innerText = 'Time Off Dates';
+    row.appendChild(heading2);
 
-    var heading = document.createElement('th');
-    heading.innerText = 'Type';
-    row.appendChild(heading);
+    const heading3 = document.createElement('th');
+    heading3.innerText = 'Type';
+    row.appendChild(heading3);
 
-    var heading = document.createElement('th');
-    heading.innerText = 'Paid';
-    row.appendChild(heading);
+    const heading4 = document.createElement('th');
+    heading4.innerText = 'Paid';
+    row.appendChild(heading4);
 
-    var heading = document.createElement('th');
-    heading.innerText = 'Reason';
-    row.appendChild(heading);
+    const heading5 = document.createElement('th');
+    heading5.innerText = 'Reason';
+    row.appendChild(heading5);
 
-    var heading = document.createElement('th');
-    heading.innerText = 'Status';
-    row.appendChild(heading);
+    const heading6 = document.createElement('th');
+    heading6.innerText = 'Status';
+    row.appendChild(heading6);
 
     timeoffs_table.appendChild(row);
 
-    for(var i = 0; i < timeoffs.timeoff.length; i++) {
+    const formatter = new Intl.DateTimeFormat('en-GB', {
+        day: '2-digit',
+        month: 'short',
+        year: 'numeric'
+    });
+
+    for(const i = 0; i < timeoffs.timeoff.length; i++) {
         if (employees.value == 0 || employees.value == timeoffs.timeoff[i].employee_id) {
             if (status.value == -1 || status.value == timeoffs.timeoff[i].approved) {
-                var row = document.createElement('tr');
+                const row = document.createElement('tr');
 
-                var column = document.createElement('td');
-                column.innerText = getEmployeeNameById(timeoffs.timeoff[i].employee_id);
-                row.appendChild(column);
+                const column1 = document.createElement('td');
+                column1.innerText = getEmployeeNameById(timeoffs.timeoff[i].employee_id);
+                row.appendChild(column1);
             
-                var column = document.createElement('td');
+                const column2 = document.createElement('td');
                 var startDate = new Date(removeZuluTime(timeoffs.timeoff[i].start_date));
                 var startDateDay = dayNames[startDate.getDay()];
                 var endDate = new Date(removeZuluTime(timeoffs.timeoff[i].end_date));
                 var endDateDay = dayNames[endDate.getDay()];
-                var timeOffDates = '' + getDbFormat(startDate) + ' (' + startDateDay + ') to ' + getDbFormat(endDate) + ' (' + endDateDay + ')';
-                column.innerText = timeOffDates; 
-                row.appendChild(column);
+                //var timeOffDates = '' + getDbFormat(startDate) + ' (' + startDateDay + ') to ' + getDbFormat(endDate) + ' (' + endDateDay + ')';
+                const timeOffDates = `${formatter.format(startDate)} (${startDateDay}) to ${formatter.format(endDate)} (${endDateDay})`;
+
+                column2.innerText = timeOffDates; 
+                row.appendChild(column2);
             
-                var column = document.createElement('td');
-                column.innerText =  timeoffs.timeoff[i].role;
-                row.appendChild(column);
+                const column3 = document.createElement('td');
+                column3.innerText =  timeoffs.timeoff[i].role;
+                row.appendChild(column3);
             
-                var column = document.createElement('td');
-                column.innerText =  YesOrNo(timeoffs.timeoff[i].paid);
-                row.appendChild(column);
+                const column4 = document.createElement('td');
+                column4.innerText =  YesOrNo(timeoffs.timeoff[i].paid);
+                row.appendChild(column4);
             
-                var column = document.createElement('td');
-                column.innerText =  timeoffs.timeoff[i].reason;
-                row.appendChild(column);
+                const column5 = document.createElement('td');
+                column5.innerText =  timeoffs.timeoff[i].reason;
+                row.appendChild(column5);
             
-                var column = document.createElement('td');
+                const column6 = document.createElement('td');
 
                 if (timeoffs.timeoff[i].approved == 0) {
-                    column.innerHTML =  'Pending... <button type="button" onclick="update(' + timeoffs.timeoff[i].id + ',' + timeoffs.timeoff[i].employee_id + ', 1' + ');">Approve</button>';
+                    column6.innerHTML =  'Pending... <button type="button" onclick="update(' + timeoffs.timeoff[i].id + ',' + timeoffs.timeoff[i].employee_id + ', 1' + ');">Approve</button>';
                 } else if (timeoffs.timeoff[i].approved == 1) {
-                    column.innerHTML =  'Approved... <button type="button" onclick="update(' + timeoffs.timeoff[i].id + ',' + timeoffs.timeoff[i].employee_id + ', 0' + ');">Reset Approval...</button>';
+                    column6.innerHTML =  'Approved... <button type="button" onclick="update(' + timeoffs.timeoff[i].id + ',' + timeoffs.timeoff[i].employee_id + ', 0' + ');">Reset Approval...</button>';
                 } else if (timeoffs.timeoff[i].approved == 2) {
-                    column.innerText =  'Unapproved: ' + timeoffs.timeoff[i].unapproved_reason;
+                    column6.innerText =  'Unapproved: ' + timeoffs.timeoff[i].unapproved_reason;
                 }
 
-                row.appendChild(column);
+                row.appendChild(column6);
             
                 timeoffs_table.appendChild(row);
             }
